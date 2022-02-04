fc8dd764c0df706dc1a640e84d055e5f37b28376
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-04 23:02:47 +0900

    [Impot Error]

--- sample/settings.py
+++ sample/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sampleapp1.apps.Sampleapp1Config',
 ]
 
 MIDDLEWARE = [
@@ -54,9 +55,7 @@ ROOT_URLCONF = 'sample.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR, 'templates',
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sample/urls.py
+++ sample/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import test
+from django.urls import path, include
+# from . import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', test.test, name='test'),
+    # path('test/', test.test, name='test'),
+    path('', include('sampleapp1.urls')),
 ]
